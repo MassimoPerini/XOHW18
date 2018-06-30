e6b88ebd200a7ccf4669037908751e5456272869
Massimo Perini <perinimassimo1995@gmail.com>
2018-06-30 23:35:40 +0200

    Removed bad files

--- firmware_arduino/Total_firmware.ino
+++ firmware_arduino/Total_firmware.ino
@@ -1,5 +1,4 @@
 #include <Wire.h>
-#include<Servo.h>
 #include <Stepper.h>
 
 const int stepsPerRevolution = 200;  //Resolution of stepper motor = 1.8 deg
@@ -15,17 +14,17 @@ void setup() {
   pinMode(3, OUTPUT); //Set pin Enable phase 2 to OUTPUT Stepper2
   pinMode(12, OUTPUT); //Set pin Enable phase 1 to OUTPUT Stepper1
   pinMode(13, OUTPUT); //Set pin Enable phase 2 to OUTPUT Stepper1
-  myStepper1.setSpeed(100); //Set speed of stepper motor
-  myStepper2.setSpeed(100); //Set speed of stepper motor
+  myStepper1.setSpeed(100); //Set speed of stepper motor Stepper1
+  myStepper2.setSpeed(100); //Set speed of stepper motor Stepper2
   Wire.begin(x); //  join i2c bus as slave, connected to PYNQ with address x . There are three slaves with : #5, #6, #7 address. x depends on the slave used.
   Wire.onReceive(receiveEvent); // register event from PYNQ
-  Serial.begin(9600); //Init Serial Port to see result ----- 9600 is bound rate
+  Serial.begin(9600); //Init Serial Port to see result ----- 9600 is baud rate
 }
 
 // ==========================Clarification===========================
-// Motors use a big quantity of current. We controll motors's pins after and before the movement to avoid overheating situation, 
+// Motors use a big quantity of current. We control motors's pins after and before the movement to avoid overheating situation, 
 // so we don't set pins value in setUp() function .
-// Without this solution, everytime we stop the motors to make some operations, it is not possible to controll the current.
+// Without this solution, everytime we stop the motors to make some operations, it is not possible to control the current.
 
 
 
@@ -52,18 +51,17 @@ void loop() {
   // I2C function to receive data
   void receiveEvent(int howMany) {
     while (1 < Wire.available()) { // loop through all but the last
+      
        c = Wire.read();     //receive byte as a character
-      Serial.print(c);         //print the character
+      Serial.print(c);      //print the character
     }
-     c = Wire.read();     //receive byte as a character
-      Serial.print(c);           // print the integer
+      c = Wire.read();     
+      Serial.print(c);    
   }
 
 
-
-
   void Stepper1Clockwise(){
-    Serial.println("clockwise_stepper");
+    Serial.println("clockwise_stepper1");
     digitalWrite(12, HIGH);  // Put Enable 1 pin to high 
     digitalWrite(13, HIGH);  // Put Enable 2 pin to high 
     myStepper1.step(50); // 90/1.8 = 50 
@@ -74,7 +72,7 @@ void loop() {
   }
 
   void Stepper1Counterclockwise(){
-    Serial.println("counterclockwise_stepper");
+    Serial.println("counterclockwise_stepper1");
     digitalWrite(12, HIGH); // Put Enable 1 pin to high 
     digitalWrite(13, HIGH); // Put Enable 2 pin to high 
     myStepper1.step(-50); // 90/1.8 = 50 
@@ -85,7 +83,7 @@ void loop() {
   }
 
   void Stepper2Clockwise(){
-    Serial.println("clockwise_stepper");
+    Serial.println("clockwise_stepper2");
     digitalWrite(2, HIGH);  // Put Enable 1 pin to high 
     digitalWrite(3, HIGH);  // Put Enable 2 pin to high 
     myStepper2.step(50); // 90/1.8 = 50 
@@ -96,7 +94,7 @@ void loop() {
   }
 
   void Stepper2Counterclockwise(){
-    Serial.println("counterclockwise_stepper");
+    Serial.println("counterclockwise_stepper2");
     digitalWrite(2, HIGH); // Put Enable 1 pin to high 
     digitalWrite(3, HIGH); // Put Enable 2 pin to high 
     myStepper2.step(-50); // 90/1.8 = 50 
